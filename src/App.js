ecc29809e6fdaec674674def71392282d1276558
MatheusGrell <45364740+MatheusGrell@users.noreply.github.com>
2019-04-21 15:29:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = { jogada: 9 };
-    this.state = { arrCasa: [0, 0, 0, 0, 0, 0, 0, 0, 0] };
+    this.state = { arrCasa: [0, 0, 0, 0, 0, 0, 0, 0, 0], jogada: 9, aux: true, value: 'R E I N I C I A R'};
+    // this.setState({arrCasa: [0,0,0,0,0,0,0,0,0]});
+    console.log(this.state);
   }
+
+
 
   componentDidMount() {
     this.updateCanvas(this.returnContext());
@@ -18,6 +21,89 @@ class App extends Component {
     return ctx;
   }
 
+  verificaGanhador() {
+    debugger;
+    setTimeout(() => {
+      if (this.state.arrCasa[0] === -1 && this.state.arrCasa[1] === -1 && this.state.arrCasa[2] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[0] === -1 && this.state.arrCasa[3] === -1 && this.state.arrCasa[6] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[0] === -1 && this.state.arrCasa[4] === -1 && this.state.arrCasa[8] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[3] === -1 && this.state.arrCasa[4] === -1 && this.state.arrCasa[5] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[6] === -1 && this.state.arrCasa[7] === -1 && this.state.arrCasa[8] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[1] === -1 && this.state.arrCasa[4] === -1 && this.state.arrCasa[7] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[2] === -1 && this.state.arrCasa[5] === -1 && this.state.arrCasa[8] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[2] === -1 && this.state.arrCasa[4] === -1 && this.state.arrCasa[6] === -1) {
+        alert('Parabéns X, você é demais!');
+        this.setState({ aux: false });
+        return;
+      }
+    }, 20);
+
+    setTimeout(() => {
+      if (this.state.arrCasa[0] === 1 && this.state.arrCasa[1] === 1 && this.state.arrCasa[2] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[0] === 1 && this.state.arrCasa[3] === 1 && this.state.arrCasa[6] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[0] === 1 && this.state.arrCasa[4] === 1 && this.state.arrCasa[8] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[3] === 1 && this.state.arrCasa[4] === 1 && this.state.arrCasa[5] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[6] === 1 && this.state.arrCasa[7] === 1 && this.state.arrCasa[8] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[1] === 1 && this.state.arrCasa[4] === 1 && this.state.arrCasa[7] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[2] === 1 && this.state.arrCasa[5] === 1 && this.state.arrCasa[8] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      } else if (this.state.arrCasa[2] === 1 && this.state.arrCasa[4] === 1 && this.state.arrCasa[6] === 1) {
+        alert('Parabéns amigo Bola, você ganhou');
+        this.setState({ aux: false });
+        return;
+      }
+    }, 20);
+
+
+    setTimeout(() => {
+      if (this.state.jogada === 0 && this.state.aux === true) {
+        alert('empatou');
+        this.setState({ aux: false });
+      }
+
+    }, 30);
+  }
   updateCanvas(ctx) {
 
     ctx.fillStyle = 'lightgray';
@@ -36,100 +122,132 @@ class App extends Component {
     ctx.stroke();
 
   }
-
   posicao(evento) {
     var tela = document.querySelector('canvas');
     var x = evento.pageX - tela.offsetLeft;
     var y = evento.pageY - tela.offsetTop;
-    if (x > 0 && x < 150 && y > 0 && y < 150 && this.state.arrCasa[0] === 0) {
-      x = 75;
-      y = 75;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.setState({arrCasa: this.state.arrCasa[0] = 1});
-      } else {
-        this.setState({arrCasa: this.state.arrCasa[0] = -1});
+
+    var clone = this.state.arrCasa.slice(0);
+    if (this.state.aux === true) {
+      if (x > 0 && x < 150 && y > 0 && y < 150 && this.state.arrCasa[0] === 0) {
+        x = 75;
+        y = 75;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[0] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[0] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 0 && x < 150 && y > 150 && y < 300 && this.state.arrCasa[1] === 0) {
-      x = 75;
-      y = 225;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[1] = 1;
-      } else {
-        this.state.arrCasa[1] = -1;
+      if (x > 0 && x < 150 && y > 150 && y < 300 && this.state.arrCasa[1] === 0) {
+        x = 75;
+        y = 225;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[1] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[1] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 0 && x < 150 && y > 300 && y < 450 && this.state.arrCasa[2] === 0) {
-      x = 75;
-      y = 375;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[2] = 1;
-      } else {
-        this.state.arrCasa[2] = -1;
+      if (x > 0 && x < 150 && y > 300 && y < 450 && this.state.arrCasa[2] === 0) {
+        x = 75;
+        y = 375;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[2] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[2] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 150 && x < 300 && y > 0 && y < 150 && this.state.arrCasa[3] === 0) {
-      x = 225;
-      y = 75;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[3] = 1;
-      } else {
-        this.state.arrCasa[3] = -1;
+      if (x > 150 && x < 300 && y > 0 && y < 150 && this.state.arrCasa[3] === 0) {
+        x = 225;
+        y = 75;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[3] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[3] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 150 && x < 300 && y > 150 && y < 300 && this.state.arrCasa[4] === 0) {
-      x = 225;
-      y = 225;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[4] = 1;
-      } else {
-        this.state.arrCasa[4] = -1;
+      if (x > 150 && x < 300 && y > 150 && y < 300 && this.state.arrCasa[4] === 0) {
+        x = 225;
+        y = 225;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[4] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[4] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 150 && x < 300 && y > 300 && y < 450 && this.state.arrCasa[5] === 0) {
-      x = 225;
-      y = 375;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[5] = 1;
-      } else {
-        this.state.arrCasa[5] = -1;
+      if (x > 150 && x < 300 && y > 300 && y < 450 && this.state.arrCasa[5] === 0) {
+        x = 225;
+        y = 375;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[5] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[5] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 300 && x < 450 && y > 0 && y < 150 && this.state.arrCasa[6] === 0) {
-      x = 375;
-      y = 75;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[6] = 1;
-      } else {
-        this.state.arrCasa[6] = -1;
+      if (x > 300 && x < 450 && y > 0 && y < 150 && this.state.arrCasa[6] === 0) {
+        x = 375;
+        y = 75;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[6] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[6] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 300 && x < 450 && y > 150 && y < 300 && this.state.arrCasa[7] === 0) {
-      x = 375;
-      y = 225;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[7] = 1;
-      } else {
-        this.state.arrCasa[7] = -1;
+      if (x > 300 && x < 450 && y > 150 && y < 300 && this.state.arrCasa[7] === 0) {
+        x = 375;
+        y = 225;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[7] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0){
+          clone[7] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
-    }
-    if (x > 300 && x < 450 && y > 300 && y < 450 && this.state.arrCasa[8] === 0) {
-      x = 375;
-      y = 375;
-      this.desenha(this.returnContext(), x, y);
-      if (this.jogada % 2 === 1) {
-        this.state.arrCasa[8] = 1;
-      } else {
-        this.state.arrCasa[8] = -1;
+      if (x > 300 && x < 450 && y > 300 && y < 450 && this.state.arrCasa[8] === 0) {
+        x = 375;
+        y = 375;
+        this.desenha(this.returnContext(), x, y);
+        this.verificaGanhador();
+        if (this.state.jogada % 2 === 1) {
+          clone[8] = 1;
+          this.setState({ arrCasa: clone });
+        } else if(this.state.jogada % 2 === 0) {
+          clone[8] = -1;
+          this.setState({ arrCasa: clone });
+        }
       }
+    } else {
+      alert('Jogo finalizado. Favor reiniciar o jogo');
     }
   }
   desenha(ctx, x, y) {
@@ -152,9 +270,9 @@ class App extends Component {
   }
 
   render() {
-    return (
+    return (        
       <canvas ref="canvas" width={450} height={450} onClick={(e) => this.posicao(e, this.returnContext())} />
-    );
+      );
   }
 }
 //ReactDOM.render(<App/>, document.getElementById('container'));
